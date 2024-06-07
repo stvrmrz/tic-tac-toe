8cb05d621617ce373a0bad323f51149fdfb3b670
Steve <stvchrm@steves-mbp.lan>
2024-06-06 19:59:16 -0600

    create helper functions for abstaction

---
+++ cypress/e2e/smoke-test.cy.js
@@ -0,0 +1,72 @@
+describe('Tic Tac Toe Game Functional Tests', () => {
+  
+  // Test to check if the game page loads successfully
+  it('should be able to load the game page', () => {
+    cy.visitGamePage();
+    cy.contains('Play').should('be.visible');
+  });
+
+  // Test to start a game and make an initial move
+  it('should be able to start a game and make a move', () => {
+    cy.visitGamePage();
+    cy.startGame('2');
+
+    // Make a move and check it was made
+    cy.makeMove('0', 'X');
+  });
+
+  // Test to simulate a win as Player X
+  it('should be able to win a game as Player X', () => {
+    cy.visitGamePage();
+    cy.startGame('2');
+
+    // Player X makes the first move
+    cy.makeMove('0', 'X');
+
+    // Player O makes a move
+    cy.makeMove('1', 'O');
+
+    // Player X makes the winning move
+    cy.makeMove('2', 'X');
+
+    // Verify the endgame alert is displayed
+    cy.verifyEndgameAlert();
+  });
+
+  // Test to simulate a win as Player O
+  it('should be able to win a game as Player O', () => {
+    cy.visitGamePage();
+    cy.startGame('3');
+
+    // Player X makes the first move
+    cy.makeMove('0', 'X');
+
+    // Player O makes a move
+    cy.makeMove('1', 'O');
+
+    // Player X makes a move
+    cy.makeMove('2', 'X');
+
+    // Player O makes a move
+    cy.makeMove('4', 'O');
+
+    // Player X makes a move
+    cy.makeMove('5', 'X');
+
+    // Player O makes the winning move
+    cy.makeMove('7', 'O');
+
+    // Verify the endgame alert is displayed
+    cy.verifyEndgameAlert();
+  });
+
+  // Test to ensure the game doesn't start with invalid input
+  it('should not be able to start a game with invalid user input', () => {
+    cy.visitGamePage();
+    cy.get('#number').focus().type('foobar');
+    cy.contains('Play').click();
+
+    // Verify the game board is not visible
+    cy.get('#table').should('not.be.visible');
+  });
+});
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,27 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+// Custom command to visit the game page
+Cypress.Commands.add('visitGamePage', () => {
+    // Visits the specified URL
+    cy.visit('https://roomy-fire-houseboat.glitch.me/');
+});
+
+// Custom command to start the game
+Cypress.Commands.add('startGame', (gameBoardNumber) => {
+    // Focus on the input field, type the game board number, and click the 'Play' button
+    cy.get('#number').focus().type(gameBoardNumber);
+    cy.contains('Play').click();
+    // Assert that the game table is visible
+    cy.get('#table').should('be.visible');
+});
+
+// Custom command to make a move in the game
+Cypress.Commands.add('makeMove', (cellId, playerSymbol) => {
+    // Click on the specified cell and check that the table contains the player symbol
+    cy.get(`#${cellId}`).click();
+    cy.get('#table').should('contain.text', playerSymbol);
+});
+
+// Custom command to verify the endgame alert
+Cypress.Commands.add('verifyEndgameAlert', () => {
+    // Check that the endgame alert is visible and contains the text 'Congratulations'
+    cy.get('#endgame').should('be.visible').should('contain.text', 'Congratulations');
+});
